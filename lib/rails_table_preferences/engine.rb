3581e63d9956a3397106b6bb2e4cc3f9a1ab4b53
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:24:16 +0900

    feat: add legacy ColumnAdjustment importer

--- lib/rails_table_preferences/engine.rb
+++ lib/rails_table_preferences/engine.rb
@@ -6,6 +6,10 @@ module RailsTablePreferences
   class Engine < ::Rails::Engine
     isolate_namespace RailsTablePreferences
 
+    rake_tasks do
+      load File.expand_path("../../tasks/rails_table_preferences.rake", __dir__)
+    end
+
     initializer "rails_table_preferences.helpers" do
       ActiveSupport.on_load(:action_controller_base) do
         helper RailsTablePreferences::TablePreferencesHelper if respond_to?(:helper)
