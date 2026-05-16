0b6655fb2ee24beb3d0e392b53554b734cd91521
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:27:31 +0900

    feat: add preset management API

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@
 
 RailsTablePreferences::Engine.routes.draw do
   scope "preferences/:table_key" do
+    get "/", to: "preferences#index", as: :preferences
+    post "/", to: "preferences#create"
     get "/(:name)", to: "preferences#show", as: :preference
     patch "/(:name)", to: "preferences#update"
     put "/(:name)", to: "preferences#update"
+    delete "/(:name)", to: "preferences#destroy"
   end
 end
