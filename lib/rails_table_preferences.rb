9c4ee7f8cdfe9f2dfbd7c4b5de9b707f02813419
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 05:57:53 +0900

    feat: add table preference persistence foundation

--- lib/rails_table_preferences.rb
+++ lib/rails_table_preferences.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 require "rails_table_preferences/version"
+require "rails_table_preferences/configuration"
 require "rails_table_preferences/engine"
 
 module RailsTablePreferences
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
 end
