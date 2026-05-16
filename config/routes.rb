19c4cf3ce0356de37ad11906d05132dda20be873
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:02:49 +0900

    feat: add preferences JSON API foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 RailsTablePreferences::Engine.routes.draw do
+  scope "preferences/:table_key" do
+    get "/(:name)", to: "preferences#show", as: :preference
+    patch "/(:name)", to: "preferences#update"
+    put "/(:name)", to: "preferences#update"
+  end
 end
