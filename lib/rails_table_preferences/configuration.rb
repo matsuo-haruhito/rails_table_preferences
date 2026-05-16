16426ddf550eac3a0988dd8ba2071a66bcca601a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:01:18 +0900

    feat: add preferences JSON API foundation

--- lib/rails_table_preferences/configuration.rb
+++ lib/rails_table_preferences/configuration.rb
@@ -2,12 +2,18 @@
 
 module RailsTablePreferences
   class Configuration
-    attr_accessor :table_name, :user_class_name, :user_foreign_key
+    attr_accessor :table_name,
+                  :user_class_name,
+                  :user_foreign_key,
+                  :parent_controller_class_name,
+                  :current_user_method
 
     def initialize
       @table_name = "table_preferences"
       @user_class_name = "User"
       @user_foreign_key = "user_id"
+      @parent_controller_class_name = "ApplicationController"
+      @current_user_method = :current_user
     end
   end
 end
