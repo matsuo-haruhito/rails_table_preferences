a59614bfa3293c39e3765c0fee7d48439631bb02
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:03:22 +0900

    feat: add preferences view helper foundation

--- lib/rails_table_preferences/engine.rb
+++ lib/rails_table_preferences/engine.rb
@@ -5,5 +5,11 @@ require "rails"
 module RailsTablePreferences
   class Engine < ::Rails::Engine
     isolate_namespace RailsTablePreferences
+
+    initializer "rails_table_preferences.helpers" do
+      ActiveSupport.on_load(:action_controller_base) do
+        helper RailsTablePreferences::TablePreferencesHelper if respond_to?(:helper)
+      end
+    end
   end
 end
