25883a7cf3bf05c2aa03f5c4cb799532fc32188d
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 06:05:40 +0900

    fix: make preference API path configurable

--- app/helpers/rails_table_preferences/table_preferences_helper.rb
+++ app/helpers/rails_table_preferences/table_preferences_helper.rb
@@ -7,7 +7,7 @@ module RailsTablePreferences
         controller: "rails-table-preferences",
         rails_table_preferences_table_key_value: table_key.to_s,
         rails_table_preferences_name_value: name.to_s,
-        rails_table_preferences_url_value: preference_path(table_key: table_key, name: name),
+        rails_table_preferences_url_value: table_preferences_preference_url(table_key: table_key, name: name),
         rails_table_preferences_settings_value: SettingsNormalizer.call(settings || {}).to_json
       }
     end
@@ -18,6 +18,14 @@ module RailsTablePreferences
       )
 
       tag.table(**options, &block)
+    end
+
+    def table_preferences_preference_url(table_key:, name: "default")
+      mount_path = RailsTablePreferences.configuration.mount_path.to_s.chomp("/")
+      encoded_table_key = ERB::Util.url_encode(table_key.to_s)
+      encoded_name = ERB::Util.url_encode(name.to_s)
+
+      "#{mount_path}/preferences/#{encoded_table_key}/#{encoded_name}"
     end
   end
 end
