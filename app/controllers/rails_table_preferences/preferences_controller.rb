f7b7621e7b71665762854f7f3a0d8b58d2381f0d
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 21:12:04 +0900

    feat: support scoped preferences in API

--- app/controllers/rails_table_preferences/preferences_controller.rb
+++ app/controllers/rails_table_preferences/preferences_controller.rb
@@ -3,9 +3,11 @@
 module RailsTablePreferences
   class PreferencesController < ApplicationController
     def index
-      preferences = Preference.for_user(table_preferences_current_user)
-                              .for_table(params[:table_key])
-                              .order(default_flag: :desc, name: :asc)
+      preferences = Preference.available_to(
+        user: table_preferences_current_user,
+        scope_context: table_preferences_scope_context
+      ).for_table(params[:table_key])
+       .order(default_flag: :desc, name: :asc)
 
       render json: {
         table_key: params[:table_key].to_s,
@@ -14,14 +16,16 @@ module RailsTablePreferences
     end
 
     def show
-      preference = Preference.find_for(user: table_preferences_current_user, table_key: params[:table_key], name: preference_name)
+      preference = resolved_preference
 
       render json: preference_payload(preference)
     end
 
     def create
       preference = Preference.new(
-        user: table_preferences_current_user,
+        user: owner_for_write_scope,
+        scope_type: scope_type_param,
+        scope_key: scope_key_param,
         table_key: params[:table_key].to_s,
         name: preference_name,
         settings: SettingsNormalizer.call(settings_params),
@@ -37,8 +41,13 @@ module RailsTablePreferences
       preference = Preference.find_or_initialize_for(
         user: table_preferences_current_user,
         table_key: params[:table_key],
-        name: preference_name
+        name: preference_name,
+        scope_type: scope_type_param,
+        scope_key: scope_key_param
       )
+      preference.user = owner_for_write_scope
+      preference.scope_type = scope_type_param
+      preference.scope_key = scope_key_param
       preference.settings = SettingsNormalizer.call(settings_params)
       preference.default_flag = default_param? if params.key?(:default)
       clear_other_defaults(preference) if preference.default_flag?
@@ -48,7 +57,13 @@ module RailsTablePreferences
     end
 
     def destroy
-      preference = Preference.find_for(user: table_preferences_current_user, table_key: params[:table_key], name: preference_name)
+      preference = Preference.find_for(
+        user: table_preferences_current_user,
+        table_key: params[:table_key],
+        name: preference_name,
+        scope_type: scope_type_param,
+        scope_key: scope_key_param
+      )
       preference&.destroy!
 
       head :no_content
@@ -56,8 +71,54 @@ module RailsTablePreferences
 
     private
 
+    def resolved_preference
+      return explicitly_scoped_preference if explicit_scope_param?
+      return default_preference if preference_name == "default"
+
+      Preference.available_named_preference(
+        user: table_preferences_current_user,
+        table_key: params[:table_key],
+        name: preference_name,
+        scope_context: table_preferences_scope_context
+      )
+    end
+
+    def explicitly_scoped_preference
+      Preference.find_for(
+        user: table_preferences_current_user,
+        table_key: params[:table_key],
+        name: preference_name,
+        scope_type: scope_type_param,
+        scope_key: scope_key_param
+      )
+    end
+
+    def default_preference
+      Preference.default_for(
+        user: table_preferences_current_user,
+        table_key: params[:table_key],
+        scope_context: table_preferences_scope_context
+      )
+    end
+
     def preference_name
       params[:name].presence || params[:preference_name].presence || "default"
+    end
+
+    def scope_type_param
+      params[:scope_type].presence || Preference::OWNER_SCOPE_TYPE
+    end
+
+    def scope_key_param
+      params[:scope_key].presence
+    end
+
+    def explicit_scope_param?
+      params[:scope_type].present? || params[:scope_key].present?
+    end
+
+    def owner_for_write_scope
+      scope_type_param == Preference::OWNER_SCOPE_TYPE ? table_preferences_current_user : nil
     end
 
     def default_param?
@@ -80,7 +141,8 @@ module RailsTablePreferences
     end
 
     def clear_other_defaults(preference)
-      Preference.for_user(table_preferences_current_user)
+      Preference.for_scope(preference.scope_type, preference.scope_key)
+                .where(RailsTablePreferences.configuration.user_foreign_key => preference.user_id)
                 .for_table(preference.table_key)
                 .where.not(id: preference.id)
                 .update_all(default_flag: false)
@@ -93,6 +155,10 @@ module RailsTablePreferences
         table_key: params[:table_key].to_s,
         name: preference&.name || preference_name,
         default: preference&.default_flag || false,
+        scope_type: preference&.scope_type || scope_type_param,
+        scope_key: preference&.scope_key,
+        scope_label: preference&.scope_label || scope_type_param,
+        editable: preference ? preference.editable_by_owner?(table_preferences_current_user) : true,
         settings: settings
       }
     end
