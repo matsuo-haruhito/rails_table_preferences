89a2521d3f21049984759cacfcbc421a70b304de
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 17:04:01 +0900

    fix: avoid preferences route helper collision

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 
 RailsTablePreferences::Engine.routes.draw do
   scope "preferences/:table_key" do
-    get "/", to: "preferences#index", as: :preferences
-    post "/", to: "preferences#create", as: :create_preference
-    get "/:name", to: "preferences#show", as: :preference
-    patch "/:name", to: "preferences#update", as: :update_preference
-    put "/:name", to: "preferences#update", as: :replace_preference
-    delete "/:name", to: "preferences#destroy", as: :destroy_preference
+    get "/", to: "preferences#index", as: :table_preferences_index
+    post "/", to: "preferences#create", as: :table_preferences_create
+    get "/:name", to: "preferences#show", as: :table_preference
+    patch "/:name", to: "preferences#update", as: :table_preference_update
+    put "/:name", to: "preferences#update", as: :table_preference_replace
+    delete "/:name", to: "preferences#destroy", as: :table_preference_destroy
   end
 end
