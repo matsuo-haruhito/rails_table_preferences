8d5e8eb0a063a1984fdb05c597d8d3559d140954
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 09:08:54 +0900

    test: cover JavaScript generator

---
+++ spec/generators/rails_table_preferences/javascript_generator_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "generators/rails_table_preferences/javascript/javascript_generator"
+require "rails/generators/testing/behavior"
+
+RSpec.describe RailsTablePreferences::Generators::JavascriptGenerator, type: :generator do
+  include Rails::Generators::Testing::Behavior
+
+  tests described_class
+  destination File.expand_path("../../tmp/generators/javascript", __dir__)
+
+  before do
+    prepare_destination
+  end
+
+  it "copies the Stimulus controller into the host application" do
+    run_generator
+
+    controller_path = file("app/javascript/controllers/rails_table_preferences_controller.js")
+    expect(controller_path).to exist
+    expect(controller_path.read).to include("export default class extends Controller")
+    expect(controller_path.read).to include("static targets = [\"editorRows\", \"presetName\", \"presetSelect\", \"defaultPreset\"]")
+  end
+end
