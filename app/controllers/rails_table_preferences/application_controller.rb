8b53b8609c387a31a5f771d2a6887c04f1ce0404
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 21:07:12 +0900

    feat: expose preference scope context to controllers

--- app/controllers/rails_table_preferences/application_controller.rb
+++ app/controllers/rails_table_preferences/application_controller.rb
@@ -5,7 +5,16 @@ module RailsTablePreferences
     private
 
     def table_preferences_current_user
-      public_send(RailsTablePreferences.configuration.current_user_method)
+      send(RailsTablePreferences.configuration.current_user_method)
+    end
+
+    def table_preferences_scope_context
+      method_name = RailsTablePreferences.configuration.scope_context_method
+      return {} if method_name.blank?
+      return {} unless respond_to?(method_name, true)
+
+      context = send(method_name)
+      context.respond_to?(:to_h) ? context.to_h : {}
     end
   end
 end
