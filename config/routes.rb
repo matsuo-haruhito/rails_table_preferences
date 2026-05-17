a62a082eb437b4452873f3a2189928e9da7b3681
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 16:59:06 +0900

    fix: use unique route helper names

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@
 RailsTablePreferences::Engine.routes.draw do
   scope "preferences/:table_key" do
     get "/", to: "preferences#index", as: :preferences
-    post "/", to: "preferences#create", as: nil
+    post "/", to: "preferences#create", as: :create_preference
     get "/:name", to: "preferences#show", as: :preference
-    patch "/:name", to: "preferences#update", as: nil
-    put "/:name", to: "preferences#update", as: nil
-    delete "/:name", to: "preferences#destroy", as: nil
+    patch "/:name", to: "preferences#update", as: :update_preference
+    put "/:name", to: "preferences#update", as: :replace_preference
+    delete "/:name", to: "preferences#destroy", as: :destroy_preference
   end
 end
