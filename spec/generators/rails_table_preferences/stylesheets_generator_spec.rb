6652ab5624f979384b4285cc77114e6a7c43af71
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 09:09:19 +0900

    test: cover stylesheets generator

---
+++ spec/generators/rails_table_preferences/stylesheets_generator_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "generators/rails_table_preferences/stylesheets/stylesheets_generator"
+require "rails/generators/testing/behavior"
+
+RSpec.describe RailsTablePreferences::Generators::StylesheetsGenerator, type: :generator do
+  include Rails::Generators::Testing::Behavior
+
+  tests described_class
+  destination File.expand_path("../../tmp/generators/stylesheets", __dir__)
+
+  before do
+    prepare_destination
+  end
+
+  it "copies the default stylesheet into the host application" do
+    run_generator
+
+    stylesheet_path = file("app/assets/stylesheets/rails_table_preferences.css")
+    expect(stylesheet_path).to exist
+    expect(stylesheet_path.read).to include(".rails-table-preferences-editor__row")
+    expect(stylesheet_path.read).to include("grid-template-columns")
+    expect(stylesheet_path.read).to include("text-overflow: ellipsis")
+  end
+end
