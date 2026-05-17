dff58b9b51e66d940f8adedcba098ae4d75c8c71
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 17:19:00 +0900

    fix: remove route helper names from engine routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 
 RailsTablePreferences::Engine.routes.draw do
   scope "preferences/:table_key" do
-    get "/", to: "preferences#index", as: :table_preferences_index
-    post "/", to: "preferences#create", as: :table_preferences_create
-    get "/:name", to: "preferences#show", as: :table_preference
-    patch "/:name", to: "preferences#update", as: :table_preference_update
-    put "/:name", to: "preferences#update", as: :table_preference_replace
-    delete "/:name", to: "preferences#destroy", as: :table_preference_destroy
+    get "/", to: "preferences#index", as: nil
+    post "/", to: "preferences#create", as: nil
+    get "/:name", to: "preferences#show", as: nil
+    patch "/:name", to: "preferences#update", as: nil
+    put "/:name", to: "preferences#update", as: nil
+    delete "/:name", to: "preferences#destroy", as: nil
   end
 end
