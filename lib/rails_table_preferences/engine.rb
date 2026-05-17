b0c8a20f13bf31fc8c862a2a0239ffe3b398342a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 16:54:22 +0900

    fix: load controller extensions before including them

--- lib/rails_table_preferences/engine.rb
+++ lib/rails_table_preferences/engine.rb
@@ -10,6 +10,11 @@ module RailsTablePreferences
       load File.expand_path("../../tasks/rails_table_preferences.rake", __dir__)
     end
 
+    initializer "rails_table_preferences.load_controller_extensions" do
+      require_dependency root.join("app/helpers/rails_table_preferences/table_preferences_helper").to_s
+      require_dependency root.join("app/controllers/concerns/rails_table_preferences/controller").to_s
+    end
+
     initializer "rails_table_preferences.helpers" do
       ActiveSupport.on_load(:action_controller_base) do
         helper RailsTablePreferences::TablePreferencesHelper if respond_to?(:helper)
