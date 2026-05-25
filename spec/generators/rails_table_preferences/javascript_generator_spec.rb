46a481f3461b6bfb470daef86f8acb18ee6ca36a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-25 14:43:23 +0900

    test: make generator specs Rails-version agnostic

--- spec/generators/rails_table_preferences/javascript_generator_spec.rb
+++ spec/generators/rails_table_preferences/javascript_generator_spec.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 require "generators/rails_table_preferences/javascript/javascript_generator"
-require "rails/generators/test_case"
 
 RSpec.describe RailsTablePreferences::Generators::JavascriptGenerator, type: :generator do
-  include Rails::Generators::Testing::Behavior
-
-  tests described_class
-  destination File.expand_path("../../tmp/generators/javascript", __dir__)
+  include FileUtils
 
   before do
     prepare_destination
@@ -21,5 +17,28 @@ RSpec.describe RailsTablePreferences::Generators::JavascriptGenerator, type: :ge
     expect(controller_path.read).to include("export default class extends Controller")
     expect(controller_path.read).to include("static targets = [\"editorRows\", \"presetName\", \"presetSelect\", \"defaultPreset\", \"status\"]")
     expect(controller_path.read).to include('loadingStatusLabel: { type: String, default: "設定を読み込み中です..." }')
+  end
+
+  def destination_root
+    File.expand_path("../../tmp/generators/javascript", __dir__)
+  end
+
+  def prepare_destination
+    rm_rf(destination_root)
+    mkdir_p(destination_root)
+  end
+
+  def run_generator(args = [])
+    with_captured_stdout do
+      described_class.start(args, destination_root: destination_root)
+    end
+  end
+
+  def with_captured_stdout
+    original_stdout = $stdout
+    $stdout = StringIO.new
+    yield
+  ensure
+    $stdout = original_stdout
   end
 end
