d41d12469ef645512277e993093e029cf254f9a2
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-25 09:30:12 +0900

    test: update generator spec for status target

--- spec/generators/rails_table_preferences/javascript_generator_spec.rb
+++ spec/generators/rails_table_preferences/javascript_generator_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe RailsTablePreferences::Generators::JavascriptGenerator, type: :ge
     controller_path = file("app/javascript/controllers/rails_table_preferences_controller.js")
     expect(controller_path).to exist
     expect(controller_path.read).to include("export default class extends Controller")
-    expect(controller_path.read).to include("static targets = [\"editorRows\", \"presetName\", \"presetSelect\", \"defaultPreset\"]")
+    expect(controller_path.read).to include("static targets = [\"editorRows\", \"presetName\", \"presetSelect\", \"defaultPreset\", \"status\"]")
+    expect(controller_path.read).to include('loadingStatusLabel: { type: String, default: "設定を読み込み中です..." }')
   end
 end
