cbad3e333cc11182463514b40c8379f3d9c6dc3c
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-27 13:43:47 +0900

    fix: load table helper extensions after helper

--- lib/rails_table_preferences/engine.rb
+++ lib/rails_table_preferences/engine.rb
@@ -11,15 +11,14 @@ module RailsTablePreferences
     end
 
     initializer "rails_table_preferences.load_controller_extensions" do
-      require_dependency root.join("app/helpers/rails_table_preferences/column_options_helper").to_s
       require_dependency root.join("app/helpers/rails_table_preferences/table_preferences_helper").to_s
+      require_dependency root.join("app/helpers/rails_table_preferences/column_options_helper").to_s
       require_dependency root.join("app/controllers/concerns/rails_table_preferences/controller").to_s
     end
 
     initializer "rails_table_preferences.helpers" do
       ActiveSupport.on_load(:action_controller_base) do
         helper RailsTablePreferences::TablePreferencesHelper if respond_to?(:helper)
-        helper RailsTablePreferences::ColumnOptionsHelper if respond_to?(:helper)
         include RailsTablePreferences::Controller
       end
     end
