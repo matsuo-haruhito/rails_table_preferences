f3a6aaf067cb76ac80369247949592ca18cc5818
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-03 04:21:20 +0900

    test: cover false hidden field roundtrip

--- spec/helpers/rails_table_preferences/table_preferences_hidden_fields_spec.rb
+++ spec/helpers/rails_table_preferences/table_preferences_hidden_fields_spec.rb
@@ -28,6 +28,28 @@ RSpec.describe RailsTablePreferences::TablePreferencesHelper, type: :helper do
       expect(html).to include('type="hidden" name="sort" value="-delivery_on"')
     end
 
+    it "renders boolean false values without emitting blank params" do
+      html = helper.table_preferences_hidden_fields(
+        settings: {
+          filters: {
+            active: { operator: :equals, value: false },
+            archived: { operator: :equals, value: true },
+            memo: { operator: :contains, value: "" }
+          }
+        },
+        columns: [
+          { key: :active, filter: { param: :active } },
+          { key: :archived, filter: { param: :archived } },
+          { key: :memo, filter: { param: :memo } }
+        ]
+      )
+
+      expect(html).to include('type="hidden" name="active" value="false"')
+      expect(html).to include('type="hidden" name="archived" value="true"')
+      expect(html).not_to include('name="memo"')
+      expect(html).not_to include('value=""')
+    end
+
     it "renders Ransack fields under the requested namespace" do
       html = helper.table_preferences_hidden_fields(
         settings: {
@@ -56,7 +78,8 @@ RSpec.describe RailsTablePreferences::TablePreferencesHelper, type: :helper do
             "filters" => {
               "customer_name_cont" => "山田",
               "memo_cont" => "",
-              "status_in" => ["未出荷", "", nil, "出荷済"]
+              "status_in" => ["未出荷", "", nil, "出荷済"],
+              "flag_in" => [true, false, "", nil]
             }
           },
           namespace: :q
@@ -66,6 +89,8 @@ RSpec.describe RailsTablePreferences::TablePreferencesHelper, type: :helper do
       expect(html).to include('type="hidden" name="q[filters][customer_name_cont]" value="山田"')
       expect(html).to include('type="hidden" name="q[filters][status_in][]" value="未出荷"')
       expect(html).to include('type="hidden" name="q[filters][status_in][]" value="出荷済"')
+      expect(html).to include('type="hidden" name="q[filters][flag_in][]" value="true"')
+      expect(html).to include('type="hidden" name="q[filters][flag_in][]" value="false"')
       expect(html).not_to include('name="q[filters][memo_cont]"')
       expect(html).not_to include('value=""')
     end
